00ba7a532be93e4c6aa64ae8b23247c3ad070fcb
ryecrow <renefang@outlook.com>
2019-11-15 00:11:53 +0800

    Use try-with-resources

--- app/src/main/java/com/young/simpledict/service/httphandler/DownloadBlobHandler.java
+++ app/src/main/java/com/young/simpledict/service/httphandler/DownloadBlobHandler.java
@@ -1,10 +1,9 @@
 package com.young.simpledict.service.httphandler;
 
-import com.young.common.IOUtil;
 import com.young.common.YLog;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.ResponseHandler;
 
 import java.io.File;
@@ -17,34 +16,31 @@ import java.io.IOException;
  * Time:   21:38
  * Life with passion. Code with creativity!
  */
-    public class DownloadBlobHandler implements ResponseHandler<File> {
-        private static final String TAG = "DownloadBlobHandler";
-        private File mFile;
+public class DownloadBlobHandler implements ResponseHandler<File> {
+    private static final String TAG = "DownloadBlobHandler";
+    private File mFile;
 
-        public DownloadBlobHandler(File fileToStore) {
-            if (fileToStore == null) {
-                throw new NullPointerException("param fileToStore can not be null");
-            }
-            mFile = fileToStore;
+    public DownloadBlobHandler(File fileToStore) {
+        if (fileToStore == null) {
+            throw new NullPointerException("param fileToStore can not be null");
         }
+        mFile = fileToStore;
+    }
 
-        @Override
-        public File handleResponse(HttpResponse httpResponse) throws ClientProtocolException, IOException {
-            int status = httpResponse.getStatusLine().getStatusCode();
-            if (status >= 200 && status < 300) {
-                HttpEntity entity = httpResponse.getEntity();
-                if (entity != null) {
-                    FileOutputStream out = new FileOutputStream(mFile);
-                    try {
-                        entity.writeTo(out);
-                    } finally {
-                        IOUtil.closeSilently(out);
-                    }
+    @Override
+    public File handleResponse(HttpResponse httpResponse) throws IOException {
+        int status = httpResponse.getStatusLine().getStatusCode();
+        if (status >= 200 && status < 300) {
+            HttpEntity entity = httpResponse.getEntity();
+            if (entity != null) {
+                try (FileOutputStream out = new FileOutputStream(mFile)) {
+                    entity.writeTo(out);
                 }
-                return mFile;
-            } else {
-                YLog.i(TAG, "Unexpected response status: " + status);
             }
-            return null;
+            return mFile;
+        } else {
+            YLog.i(TAG, "Unexpected response status: " + status);
         }
+        return null;
+    }
 }
